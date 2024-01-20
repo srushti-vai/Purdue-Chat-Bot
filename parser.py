644d4caa089ed1869ad525334e2a65a5552b680f
srushti-vai <srushtigv@gmail.com>
2024-01-20 15:49:05 -0500

    query links

--- parser.py
+++ parser.py
@@ -2,9 +2,12 @@ import requests
 
 api_key = "AIzaSyBc7i0k1FQaor4nUcAamdNdkRdLBlXQCy4"
 search_engine_id = "b33f7cf76aca9467d"
-query = "what is bgr?"
+query = "history of computer science"
 
 url = f'https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={search_engine_id}'
 
-subject_html = requests.get(url).text
-print(subject_html)
+subject_html = requests.get(url)
+data = subject_html.json()
+
+for i in data['items']:
+    print(i['link'])
