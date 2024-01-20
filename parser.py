ed1594d1695fd8001615edf3b00a82f09bdf5376
srushti-vai <srushtigv@gmail.com>
2024-01-20 18:04:03 -0500

    scraping progress

--- parser.py
+++ parser.py
@@ -1,13 +1,13 @@
 import requests
 
-api_key = "AIzaSyBc7i0k1FQaor4nUcAamdNdkRdLBlXQCy4"
-search_engine_id = "b33f7cf76aca9467d"
-query = "history of computer science"
+# api_key = "AIzaSyBc7i0k1FQaor4nUcAamdNdkRdLBlXQCy4"
+# search_engine_id = "b33f7cf76aca9467d"
+# query = "history of computer science"
 
-url = f'https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={search_engine_id}'
+# url = f'https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={search_engine_id}'
 
-subject_html = requests.get(url)
-data = subject_html.json()
+# subject_html = requests.get(url)
+# data = subject_html.json()
 
-for i in data['items']:
-    print(i['link'])
+# for i in data['items']:
+#     print(i['link'])
--- scraping.py
+++ scraping.py
@@ -1,20 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
-search = input("What would you like to search?\n")
+# search = input("What would you like to search?\n")
+# search = search.replace(" ", "+")
 
-search = search.replace(" ", "+")
-
-url = 'https://www.purdue.edu/home/search/?q=' + search
+url = 'https://www.purdue.edu/orientation/bgr/'
 
 print(url)
 
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
+page = requests.get(url).text
+soup = BeautifulSoup(page, "html.parser")
 
-doc = open("temp scraping", "w")
-doc.write(str(soup))
-doc.close
+# container_div = soup.find('div', class_='introContainer col-lg-4 col-md-4 col-sm-12 col-xs-12')
+
+container_div = soup.find_all("div", attrs={"class":"container"})
+
+for c in container_div:
+    print(c.text)
+
+# if (container_div):
+
+#     paragraphs = container_div.find_all('p')
+
+#     if paragraphs:
+
+#         for p in paragraphs:
+#             print(p.text)
+
+#     else:
+#         print("no p found")
+
+# else:
+#     print('no container div found')
 
 # search_url = soup.findall("a", attrs={"class":"gs-title"})
 
